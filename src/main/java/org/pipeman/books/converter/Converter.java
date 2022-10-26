1c77ce669b803b0b9376e2c911d025886c7b8bda
pipeman <80647470+MrRedRhino@users.noreply.github.com>
2022-10-26 17:49:16 +0200

    Improve the PDF to HTML converter

--- src/main/java/org/pipeman/books/converter/Converter.java
+++ src/main/java/org/pipeman/books/converter/Converter.java
@@ -6,6 +6,8 @@ import org.fit.pdfdom.PDFDomTree;
 import org.fit.pdfdom.PDFDomTreeConfig;
 import org.fit.pdfdom.resource.HtmlResource;
 import org.fit.pdfdom.resource.HtmlResourceHandler;
+import org.pipeman.books.BookIndex;
+import org.pipeman.books.utils.TerminalUtil;
 
 import java.io.*;
 
@@ -36,7 +38,42 @@ public class Converter {
     }
 
     public static void main(String[] args) throws IOException {
+        int bookId;
+        File pdf;
+        float compressionQuality;
 
+        while (true) {
+            System.out.print("Enter the id for the new book (must be an integer)");
+            bookId = TerminalUtil.readInt();
+            if (BookIndex.INSTANCE.books().get(bookId) == null) break;
+            System.out.println("Id is already taken");
+        }
+
+        while (true) {
+            System.out.print("Enter the path the new pdf file > ");
+            File f = new File(TerminalUtil.readLine());
+            if (f.exists()) {
+                pdf = f;
+                break;
+            }
+            System.out.println("File does not exist");
+        }
+
+        while (true) {
+            try {
+                System.out.print("Enter the compression quality (lower values produce worse results, range 0-1) > ");
+                float f = Float.parseFloat(TerminalUtil.readLine());
+                if (f < 0 || f > 1) System.out.println("Value has to be between 0 and 1");
+                else {
+                    compressionQuality = f;
+                    break;
+                }
+            } catch (NumberFormatException ignored) {
+            }
+        }
+
+        Compression.compressionQuality = compressionQuality;
+        Converter.convertBook(bookId, pdf);
     }
 
     private static class ResourceHandler implements HtmlResourceHandler {
